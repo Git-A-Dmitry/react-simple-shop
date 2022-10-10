a59980a26eedccda95a46839632fb093da1f3875
Dmitry A <adcntt@gmail.com>
2022-10-10 17:14:53 +0300

    changes in package

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import { Header } from './components/Header';
 import { Shop } from './components/Shop';
 import { Footer } from './components/Footer';
 import { Title } from './components/Title';
-// import './App.css';
 
 function App() {
   return (
