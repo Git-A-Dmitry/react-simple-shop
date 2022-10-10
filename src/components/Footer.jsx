f217e5c6ef763461fc2bfbd7c5187dac78daf239
Dmitry A <adcntt@gmail.com>
2022-10-10 16:51:42 +0300

    footer target

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
       <footer className='page-footer grey darken-2'>
         <div className='container'>
           © {new Date().getFullYear()} Hero Shop - All Rights Reserved
-          <a className='grey-text text-lighten-4 right' href='https://github.com/Git-A-Dmitry/react-simple-shop'>
+          <a className='grey-text text-lighten-4 right' href='https://github.com/Git-A-Dmitry/react-simple-shop' target='_blank' rel='noreferrer'>
             Repository
           </a>
         </div>
